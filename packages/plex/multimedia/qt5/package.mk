d81ece5bdeda111e6dfb4f4c33f63d97f048231d
LongChair <longchair@hotmail.com>
2018-05-13 13:59:41 +0200

    [LE9] qt5 : use python2
    now python is not the default package name anymore, we need to specify Python2 or Python3

--- packages/plex/multimedia/qt5/package.mk
+++ packages/plex/multimedia/qt5/package.mk
@@ -32,8 +32,8 @@ PKG_LONGDESC="Qt GUI toolkit"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_BASE_DEPENDS_TARGET="curl bzip2 Python zlib:host zlib libpng tiff dbus glib fontconfig glibc flex bison ruby libdrm atk"
-PKG_BASE_BUILD_DEPENDS_TARGET="bzip2 Python zlib:host zlib libpng tiff dbus glib fontconfig openssl linux-headers glibc pulseaudio"
+PKG_BASE_DEPENDS_TARGET="curl bzip2 Python2 zlib:host zlib libpng tiff dbus glib fontconfig glibc flex bison ruby libdrm atk"
+PKG_BASE_BUILD_DEPENDS_TARGET="bzip2 Python2 zlib:host zlib libpng tiff dbus glib fontconfig openssl linux-headers glibc pulseaudio"
 
 # determine QPA related packages
 if [ "$DISPLAYSERVER" = "x11" ]; then
